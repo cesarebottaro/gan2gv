6269bb8cf73339a4050e0c3e9a323e68bdbfbce8
Cesare Bottaro <cesare.bottaro@gmail.com>
2020-11-09 22:40:21 +0100

    inizio

--- gan2gv.bat
+++ gan2gv.bat
@@ -1,4 +1,5 @@
 @echo off
+if exist %~n1.svg del %~n1.svg
 F:\PortableApps\"Portable Python 3.2.5.1"\App\python.exe gan2gv.py %~n1 > %~n1.gv 
 F:\PortableApps\GraphvizPortable\App\graphviz\bin\dot.exe -Tsvg -o"%~n1.svg" -Kdot -q1 "%~n1.gv"
-start %~n1.svg
+if exist %~n1.svg start %~n1.svg
--- gan2gv.py
+++ gan2gv.py
@@ -8,6 +8,7 @@ if len(sys.argv) < 2:
     print("Name of file .gan is needed!")
     print("Exiting...")
     sys.exit()
+	
 filename = sys.argv[1]
 # open the xml file for reading:
 file = open(filename+".gan",'r')
@@ -18,24 +19,26 @@ file.close()
 # parse the xml you got from the file
 dom = parseString(data)
 
-print ("digraph "+filename+" {")
+print ("digraph "+filename+" {") # graph starts
 print ("  rankdir=LR;")
 print ("  graph [nodesep=.7, rankdir=LR, splines=ortho];")
 print ("  node [shape=record, width=1.5, height=.1];")
-print ("  legenda [label = \"{ES|D|EF} | {Task} | {LF|S|LF}\"]")
+print ("  legenda [label = \"{ES|D|EF} | {Task} | {LF|S|LF}\"]", end="\n\n") # legend record
+
+maxEF=datetime.date(2020, 1, 1)
 
 for node in dom.getElementsByTagName('task'):  # visit every node <task />
     print ("  node_", node.getAttribute('id'), sep="", end="")
     print (" [label = \"{", sep="", end="") # starts node description
     ES=datetime.datetime.strptime(node.getAttribute('start'), "%Y-%m-%d")
     duration=int(node.getAttribute('duration'))
-    print (ES.strftime("%d/%m/%y"), sep="", end="")
+    print (ES.strftime("%d/%m/%Y"), sep="", end="")
 
     if duration > 0:
         delta=datetime.timedelta(days=duration-1)
         print ("|", duration, "|", sep="", end="")
-        EF=ES+delta;
-        print (EF.strftime("%d/%m/%y"), sep="", end="")
+        EF=ES+delta
+        print (EF.strftime("%d/%m/%Y"), sep="", end="")
     else:
         delta=datetime.timedelta(days=0) # not used?
 
@@ -53,6 +56,11 @@ for node in dom.getElementsByTagName('task'):  # visit every node <task />
     for a in alist:
         print ("node_", a.getAttribute('id'), sep="", end=" ")
     print ("}")
+    
+print ("  legenda2 [label = \"{temporary: don't read} | {", sep="", end="")
+print (maxEF.strftime("%d/%m/%Y"), sep="", end="")
+print ("} | {# comment}\"]", sep="")
 
-print ("}")
+print ("}") # graph ends
+
 
