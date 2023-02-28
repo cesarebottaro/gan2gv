15d7bb749d8b390c22e7e8d625803be5ae6337cf
Cesare Bottaro <cesare.bottaro@gmail.com>
2023-02-28 12:10:21 +0100

    Delete gan2gv.bat

--- gan2gv.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-if exist %~n1.svg del %~n1.svg
-F:\PortableApps\"Portable Python 3.2.5.1"\App\python.exe gan2gv.py %~n1 > %~n1.gv 
-F:\PortableApps\GraphvizPortable\App\graphviz\bin\dot.exe -Tsvg -o"%~n1.svg" -Kdot -q1 "%~n1.gv"
-if exist %~n1.svg start %~n1.svg
